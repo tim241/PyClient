5ae8958d5eca983c1195741c830983be99bcd979
Tim Wanders <timwanders241@gmail.com>
2017-09-12 19:57:46 +0200

    Added config file reader/writer

--- client/client.py
+++ client/client.py
@@ -1,6 +1,35 @@
 from client.irc import IRC
-
+import configparser as ConfigParser
+import string, os, sys
 # TODO Add a config and read it from here
-
+ip = 'none'
+port = 0000
+ssl = 'none'
+nick = 'none'
+def ReadConf():
+	config = ConfigParser.ConfigParser()
+	config.read("irc.conf")
+	ip = config.get('Server', 'ip')
+	port = config.getint('Server', 'port')
+	ssl = config.get('Server', 'ssl')
+	nick = config.get('User', 'nick')
+def GenExampleConf():
+	config = ConfigParser.RawConfigParser()
+	config.add_section('Server')
+	config.add_section('User')
+	config.set('Server', 'ip', 'localhost')
+	config.set('Server', 'port', '6667')
+	config.set('Server', 'ssl', 'False')
+	config.set('User', 'nick', "ExampleUser")
+	with open('irc.conf', 'w') as configfile: 
+		config.write(configfile)
+	print("Created a template config file!")
 def init():
-	IRC.Connect("irc.freenode.net", 6697, True, "TimsPyBot")
+	if os.path.isfile('irc.conf'):
+		print("Config found!")
+		ReadConf()
+	else:
+		print("Config not found!")
+		GenExampleConf()
+		sys.exit(1)
+	IRC.Connect(ip, port, ssl, nick)
